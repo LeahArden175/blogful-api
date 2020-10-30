c674f3e6f6cad47b9e66ebd62c566ca6ffab5f5a
Leah Arden <leaharden175@gmail.com>
2020-10-30 17:04:21 -0400

    added migrations 3 and 4

--- src/articles/articles-router.js
+++ src/articles/articles-router.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const xss = require('xss')
 const ArticlesService = require('./articles-service')
@@ -21,7 +22,7 @@ articlesRouter
       const knexInstance = req.app.get('db')
     ArticlesService.getAllArticles(knexInstance)
       .then(articles => {
-        res.json(articles)
+        res.json(articles.map(serializeArticle))
       })
       .catch(next)
   })
@@ -44,7 +45,7 @@ articlesRouter
       .then(article => {
         res
           .status(201)
-          .location(`/articles/${article.id}`)
+          .location(path.posix.join(req.originalUrl, `/${article.id}`))
           .json(serializeArticle(article))
       })
       .catch(next)
@@ -68,25 +69,8 @@ articlesRouter
       })
       .catch()
   })
-   .get((req, res, next) => {
-    res.json({
-           id: res.article.id,
-           style: res.article.style,
-           title: xss(res.article.title), // sanitize title
-           content: xss(res.article.content), // sanitize content
-           date_published: res.article.date_published,
-         })
-//     const knexInstance = req.app.get('db')
-//     ArticlesService.getById(knexInstance, req.params.article_id)
-//       .then(article => {
-//         if (!article) {
-//           return res.status(404).json({
-//             error: { message: `Article doesn't exist` }
-//           })
-//         }
-//         res.json(serializeArticle(article))
-//       })
-//       .catch(next)
+  .get((req, res, next) => {
+    res.json(serializeArticle(res.article))
   })
   .delete((req, res, next) => {
       ArticlesService.deleteArticle(
@@ -94,6 +78,29 @@ articlesRouter
           req.params.article_id
       )
       .then(() => {
+          res.status(204).end()
+      })
+      .catch(next)
+  })
+  .patch(jsonParser, (req, res, next) => {
+      const { title, content, style } = req.body
+      const articleToUpdate = { title, content, style}
+
+      const numberOfValues = Object.values(articleToUpdate).filter(Boolean).length
+       if (numberOfValues === 0) {
+         return res.status(400).json({
+           error: {
+             message: `Request body must contain either 'title', 'style' or 'content'`
+           }
+         })
+       }
+
+      ArticlesService.updateArticle(
+          req.app.get('db'),
+          req.params.article_id,
+          articleToUpdate
+      )
+      .then(numRowsAffected => {
           res.status(204).end()
       })
       .catch(next)
---
+++ src/users/users-router.js
@@ -0,0 +1,7 @@
+const path = require('path')
+const express = require('express')
+const xss = require('xss')
+const UsersService = require('./users-service')
+
+const usersRouter = express.Router()
+const jsonParser = express.json()
